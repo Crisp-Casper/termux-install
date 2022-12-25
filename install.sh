a61a55510f0a05601ff79daf4c93a23e45b5385e
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2022-12-25 11:47:43 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,6 @@ echo -e "${NC}    2. A working internet connection.\n\n"
 read -p "When you are ready just press enter:"
 
 # Basic setup
-termux-change-repo
-sleep 300
 termux-setup-storage
 sleep 2
 apt-get update
