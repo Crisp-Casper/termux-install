42711cd362d5e1ef3f9ffd212c23c8622ce2042a
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-02-28 11:39:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ read -p "When you are ready just press enter:"
 # Basic setup
 termux-setup-storage
 sleep 2
-pkg install libxslt git ani-cli wget fzf aria2 -y
+pkg install libxslt git ani-cli wget fzf aria2 python-pip -y
 git clone https://github.com/pystardust/ani-cli.git
 cd ani-cli
 cp ani-cli /data/data/com.termux/files/usr/bin/ani-cli
