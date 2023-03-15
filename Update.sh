ec9db23f8fc0baa586a1cc1dafc87b2610d02266
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-03-14 18:18:30 -0600

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
-git clone https://github.com/pystardust/ani-cli.git
-cd ani-cli
-cp ani-cli /data/data/com.termux/files/usr/bin/ani-cli
-cd ..
-rm -rf ani-cli
-pkg upgrade -y
-YOUTUBEDL_CONFIG_FOLDER="${HOME}/.config/yt-dlp/"
-rm "${YOUTUBEDL_CONFIG_FOLDER}/config" -rf
-curl -L https://raw.githubusercontent.com/Crisp-Casper/termux-install/main/.ytdl.conf >"${YOUTUBEDL_CONFIG_FOLDER}/config"
+rm ${HOME}/searx -rf
+curl https://raw.githubusercontent.com/Crisp-Casper/termux-install/main/install.sh | bash
