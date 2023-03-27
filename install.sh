6814a6dc4c49c76a55faa19b79bb1c2361e2550c
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-03-27 11:45:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,12 +17,7 @@ read -p "When you are ready just press enter:"
 # Basic setup
 termux-setup-storage
 sleep 5
-rm /data/data/com.termux/files/usr/etc/tls/openssl.cnf
-rm /data/data/com.termux/files/usr/etc/motd
-rm /data/data/com.termux/files/usr/etc/motd-playstore
-rm /data/data/com.termux/files/usr/etc/profile.d/init-termux-properties.sh
-rm /data/data/com.termux/files/usr/etc/bash.bashrc
-pkg upgrade -y | yes
+yes | pkg upgrade -y
 pkg update -y
 pkg install python ffmpeg libxslt git ani-cli wget fzf aria2 python-pip rust p7zip figlet -y
 git clone https://github.com/pystardust/ani-cli.git
