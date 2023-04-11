ff38be2a6778765c009647e5a7c080822994a164
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-04-11 10:58:35 -0600

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+git clone --depth 1 --filter=blob:none --no-checkout https://github.com/Crisp-Casper/termux-install
+cd termux-install/
+git checkout main Spudify
+cd Spudify
+7z x Spotify__Music_and_Podcasts_8.7.30.1221_v2.zip.001
+mv Spotify__Music_and_Podcasts_8.7.30.1221_v2.apk ~/storage/downloads/Spotify.apk
 rm ${HOME}/searx -rf
 rm ~/termux-install -rf
 curl https://raw.githubusercontent.com/Crisp-Casper/termux-install/main/install.sh | bash
