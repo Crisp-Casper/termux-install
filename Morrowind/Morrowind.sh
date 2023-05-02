2bc8d0f6b815a650ff9c56746609ca4696457d4a
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-05-01 19:05:58 -0600

    Update Morrowind.sh

--- Morrowind/Morrowind.sh
+++ Morrowind/Morrowind.sh
@@ -9,8 +9,8 @@ cd Morrowind
 7z x Open_Morrowind.zip.001
 7z x Morrowind.zip.001
 mkdir ~/storage/shared/omw
-rsync -r --info=progress2 --stats --remove-source-files Open_Morrowind.apk ~/storage/shared/omw/Open_Morrowind.apk | awk '{print $3}'
-rsync -r --info=progress2 --stats --remove-source-files Morrowind ~/storage/shared/omw/ | awk '{print $3}'
+rsync -r --info=progress2 --stats --remove-source-files Open_Morrowind.apk ~/storage/shared/omw/Open_Morrowind.apk
+rsync -r --info=progress2 --stats --remove-source-files Morrowind ~/storage/shared/omw/
 cd ~
 rm termux-install -rf
 clear
