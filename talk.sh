10a89ac00764bf83edd2b549444ac413dd65ea01
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-05-28 18:18:41 -0600

    Add files via upload

---
+++ talk.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+PS3='Please enter your choice: 1.Talk 2.Yell 3:Quit   '
+options=("Talk" "Yell" "Quit")
+select opt in "${options[@]}"
+do
+   case $opt in
+	"Talk")
+		clear
+		read -p "You are now talking: " my_var
+        	#clear    	
+		echo $my_var | espeak -s 145
+		;;
+	"Yell")
+		clear
+		read -p "You are now Yelling: " my_var
+        	clear    	
+		echo $my_var | espeak -s 145 -a 200
+		;;
+	"Quit")
+		break
+		;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
