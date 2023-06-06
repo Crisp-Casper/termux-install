aef41ff13e541230295c9e934a1ed1b337a55a0c
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-06-06 14:13:38 -0600

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-rm termux-install -rf
 rm ${HOME}/searx -rf
-rm ~/termux-install -rf
+rm $HOME/termux-install -rf
 curl https://raw.githubusercontent.com/Crisp-Casper/termux-install/main/install.sh | bash
