b5bd1fbf8e1e4f0aa007bcc818c76a80678459db
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-07-04 10:03:33 -0600

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -62,7 +62,7 @@ echo -e "${cyan}$MENU${green}"
 				echo "ani-cli installed"
 				else
 				cd $HOME
-				git clone https://github.com/pystardust/ani-cli.git
+				git clone -b hex_decryption https://github.com/pystardust/ani-cli.git
 				cd ani-cli
 				cp ani-cli $PREFIX/bin/ani-cli
 				cd $HOME
@@ -130,7 +130,7 @@ echo -e "${cyan}$MENU${green}"
 							;;
 						"Update")
 							rm $PREFIX/bin/ani-cli -rf
-							git clone https://github.com/pystardust/ani-cli.git
+							git clone -b hex_decryption https://github.com/pystardust/ani-cli.git
 							cd ani-cli
 							cp ani-cli $PREFIX/bin/ani-cli
 							cd $HOME
