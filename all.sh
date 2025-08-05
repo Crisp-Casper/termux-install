b40435ebcc1b15f22ccea71c832258d0283c2f07
crisp-casper <Crispyboicasper@gmail.com>
2025-08-04 23:35:21 -0600

    added music list

---
+++ all.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+
+#Make folders
+mkdir /data/data/com.termux/files/home/storage/music/All
+mkdir /data/data/com.termux/files/home/storage/music/bop
+mkdir /data/data/com.termux/files/home/storage/music/catface
+mkdir /data/data/com.termux/files/home/storage/music/Ching,\ ching,\ chop-seuy
+mkdir /data/data/com.termux/files/home/storage/music/computer-wave
+mkdir /data/data/com.termux/files/home/storage/music/Happieee
+mkdir /data/data/com.termux/files/home/storage/music/Loud
+mkdir /data/data/com.termux/files/home/storage/music/Punk
+mkdir /data/data/com.termux/files/home/storage/music/stoney\ baloney
+mkdir /data/data/com.termux/files/home/storage/music/top
+mkdir /data/data/com.termux/files/home/storage/music/xtreme
+mkdir /data/data/com.termux/files/home/storage/music/Country
+mkdir /data/data/com.termux/files/home/storage/music/Radio\ Head
+mkdir /data/data/com.termux/files/home/storage/music/BBE
+
+#Add music to correct folders
+cd /data/data/com.termux/files/home/storage/music/bop; spotdl "https://open.spotify.com/playlist/1dK7v1LXSlt0710yxyNc6j?si=95a9b1880d4b4c3c" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/catface; spotdl "https://open.spotify.com/playlist/7KRotXGZn2NkE2mKIDpy4H?si=7a6382cd101843e6" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/Ching,\ ching,\ chop-seuy;\ spotdl "https://open.spotify.com/playlist/1zA5D0MShD37rDB2oX68Ud?si=e016ea294ad645d6" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/computer-wave; spotdl "https://open.spotify.com/playlist/1ookJ4NLvMLQfsOpJB9LGs?si=c65d5a95fabe4569" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/Happieee; spotdl "https://open.spotify.com/playlist/5YAjudSXrc8RrDpmF8u2Lm?si=b9c9dffd53464708" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/Loud; spotdl "https://open.spotify.com/playlist/2CFBF9ujhXg7JnxZrgaEuQ?si=5e8812d535e84f6b" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/Punk; spotdl "https://open.spotify.com/playlist/6coBQYBSJ1ORGieay91Cbj?si=ee51d6dc51284f86" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/stoney\ baloney; spotdl "https://open.spotify.com/playlist/4C6BmABgWij62W02LV4hHm?si=32dda1044e714c43" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/top; spotdl "https://open.spotify.com/playlist/0xFKMZwcciP4R4J5jwqigI?si=5f02bb5e25964b8a" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/xtreme; spotdl "https://open.spotify.com/playlist/1QH7r3NOjYPoU00HMsSWlD?si=138da0fa48e940df" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/Country; spotdl "https://open.spotify.com/playlist/7BcYLCZrQMqnDAlRruPwhp?si=8980e19b537e4222" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/Radio\ Head; spotdl "https://open.spotify.com/playlist/46Y9X4kmPmTQ5PXIahjCEw?si=eb459f6f0262492e" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
+cd /data/data/com.termux/files/home/storage/music/BBE; spotdl "https://open.spotify.com/playlist/1TukwtwR8cMbL9AGFFxq6K?si=fe670c30858d4b48" --cookie-file /data/data/com.termux/files/home/storage/music/cookies.txt
